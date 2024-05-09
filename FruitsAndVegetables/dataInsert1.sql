db1872307e9427375473ddc3b5ce7c71bc0e8866
MPutnis <martins.putnis@gmail.com>
2024-05-09 13:58:18 +0300

    finished product clasification inserts, made a new file for data inserts in more complex relations

--- FruitsAndVegetables/dataInsert1.sql
+++ FruitsAndVegetables/dataInsert1.sql
@@ -163,6 +163,20 @@ INSERT INTO KlientuAdreses VALUES
     ('Tehnika 12, Jõhvi, Ida-Viru maakond, Estonia', 'frui85')
 GO
 
+-- Piegades( RekinaNr, PiegadesDatums, PiegadatajaID)
+
+-- Pasutijumi( PasutijumaNr, DaudzumsKg, PardosanasCenaKg,
+    -- KomplektesanasDatums, KomplektetajaID, KlientaID)
+
+-- ProduktuAtlikums( Serija, Nosaukums, IepirkumaCenaKg, PiegadataisDaudzumsKg
+    --  SkirnesID, RekinaNr)
+
+-- ProduktuSadalijums( NoliktavasNumurs, ProduktaSerija, ProduktaNosaukums)
+
+-- ProduktiPasutijuma( ProduktaSerija, ProduktaNosaukums, PasutijumaNumurs)
+
+-- Rezervacijas( DarbiniekaID, ProduktaSerija, ProduktaNosaukums)
+
 -- VisparigasProduktuGrupas( Kods, Nosaukums)
 INSERT INTO VisparigasProduktuGrupas VALUES
     ('aug01', 'Augļi'),
---
+++ FruitsAndVegetables/datainsert2.sql
@@ -0,0 +1,13 @@
+-- Piegades( RekinaNr, PiegadesDatums, PiegadatajaID)
+
+-- Pasutijumi( PasutijumaNr, DaudzumsKg, PardosanasCenaKg,
+    -- KomplektesanasDatums, KomplektetajaID, KlientaID)
+
+-- ProduktuAtlikums( Serija, Nosaukums, IepirkumaCenaKg, PiegadataisDaudzumsKg
+    --  SkirnesID, RekinaNr)
+
+-- ProduktuSadalijums( NoliktavasNumurs, ProduktaSerija, ProduktaNosaukums)
+
+-- ProduktiPasutijuma( ProduktaSerija, ProduktaNosaukums, PasutijumaNumurs)
+
+-- Rezervacijas( DarbiniekaID, ProduktaSerija, ProduktaNosaukums)
